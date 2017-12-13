68b022e0320f0113a4ceec641ded36425e07e4c5
heidechv <heidechv@rose-hulman.edu>
2017-12-13 11:34:23 -0500

    m3 finished

--- src/m3_implementing_functions_that_mutate.py
+++ src/m3_implementing_functions_that_mutate.py
@@ -134,7 +134,7 @@ def swap_colors(circle, rectangle):
       :type rectangle: rg.Rectangle
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     #
     ####################################################################
@@ -145,6 +145,10 @@ def swap_colors(circle, rectangle):
     #      B = temp
     ####################################################################
     # ------------------------------------------------------------------
+
+    color = circle.fill_color
+    circle.fill_color = rectangle.fill_color
+    rectangle.fill_color = color
 
 
 # ----------------------------------------------------------------------
